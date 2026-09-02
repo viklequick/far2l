568b70fdcc2581b945b977e34cc088ae1bc03685
viklequick <viklequick@tut.by>
2026-09-02 15:13:15 +0200

    process list: added UI like far3, linux only

--- far2l/src/plist.cpp
+++ far2l/src/plist.cpp
@@ -45,6 +45,178 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "dirmix.hpp"
 #include "manager.hpp"
 
+#ifdef __linux__
+
+#include <dirent.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+
+#include <vector>
+#include <string>
+
+static int is_pid_dir(const char *name) {
+    for (const char *p = name; *p; p++)
+        if (!isdigit(*p)) return 0;
+    return 1;
+}
+
+static void enumerateProcesses(std::vector<std::wstring>& v, std::vector<int>& pidv) 
+{
+	v.clear();
+	pidv.clear();
+    struct dirent *entry;
+
+    DIR *d = opendir("/proc");
+    if (!d) return;
+
+    while ((entry = readdir(d)) != NULL) {
+        if (!is_pid_dir(entry->d_name))
+            continue;
+
+        int pid = atoi(entry->d_name);
+
+        // ---- Read process name from /proc/<pid>/comm ----
+        char path[256];
+        snprintf(path, sizeof(path), "/proc/%d/comm", pid);
+
+        FILE *f = fopen(path, "r");
+        if (!f) continue;
+
+        char name[256];
+        if (!fgets(name, sizeof(name), f)) {
+            fclose(f);
+            continue;
+        }
+        fclose(f);
+
+        // ---- Read CPU and memory from /proc/<pid>/stat ----
+        snprintf(path, sizeof(path), "/proc/%d/stat", pid);
+        f = fopen(path, "r");
+        if (!f) continue;
+
+        long utime, stime, rss;
+        char commbuf[256], state;
+
+        /*
+         * /proc/<pid>/stat format:
+         * pid (comm) state ... utime stime ... rss ...
+         */
+        fscanf(f, "%*d (%[^)]) %c", commbuf, &state);
+
+        // Skip fields until utime (14) and stime (15)
+        for (int i = 0; i < 11; i++)
+            fscanf(f, "%*s");
+
+        fscanf(f, "%ld %ld", &utime, &stime);
+
+        // Skip fields until rss (24)
+        for (int i = 0; i < 7; i++)
+            fscanf(f, "%*s");
+
+        fscanf(f, "%ld", &rss);
+        fclose(f);
+
+        long page_kb = sysconf(_SC_PAGESIZE) / 1024;
+        long rss_kb = rss * page_kb;
+
+        char* q = name + strlen(name) - 1;
+        while(q > name && isspace(*q)) --q;
+        q[1] = 0;
+
+        /*
+        printf("PID: %d\n", pid);
+        printf("Name: %s", name);
+        printf("CPU ticks: %ld (user) + %ld (system)\n", utime, stime);
+        printf("RSS: %ld KB\n", rss_kb);
+        printf("----\n");
+        */
+        long total_time = utime + stime;
+        const char* cpuLoad = "?";
+
+        if (total_time < 100)        cpuLoad = "idle";
+		else if (total_time < 1000)  cpuLoad = "low";
+		else if (total_time < 10000) cpuLoad = "medium";
+		else                         cpuLoad = "high";
+
+		FARString strStr;
+		strStr.Format(L"%6.6d %lc %-25.25s %lc %6s %lc %6ld Kb", pid, BoxSymbols[BS_V1], name, BoxSymbols[BS_V1], cpuLoad, BoxSymbols[BS_V1], rss_kb);
+		v.push_back(strStr.GetWide());
+		pidv.push_back(pid);
+    }
+
+    closedir(d);
+}
+
+void ShowProcessList()
+{
+	std::vector<std::wstring> v;
+	std::vector<int> pidv;
+	enumerateProcesses(v, pidv);
+
+	int GroupsLen = (int)v.size();
+	if (GroupsLen < 1) return; // nothing to display
+
+	MenuDataEx Groups[GroupsLen];
+	for (int j = 0; j < GroupsLen; ++j) {
+		Groups[j] = { v[j].c_str(), 0, 0 };
+	}
+
+	VMenu ProcList(Msg::ProcessListTitle, Groups, GroupsLen, ScrY - 4);
+
+	ProcList.SetPosition(-1, -1, 0, 0);
+	ProcList.SetFlags(VMENU_WRAPMODE | VMENU_NOTCHANGE);
+	ProcList.ClearDone();
+
+	ProcList.AssignHighlights(FALSE);
+	ProcList.SetBottomTitle(Msg::ProcessListBottom);
+
+	ProcList.Show();
+
+	while (!ProcList.Done()) {
+		FarKey Key = ProcList.ReadInput();
+
+		switch (Key) {
+		case KEY_F1:
+			Help::Present(L"TaskList");
+			break;
+		case KEY_CTRLR:	
+			ProcList.Hide();
+			ProcList.DeleteItems();
+
+			ProcList.SetPosition(-1,-1,0,0);
+
+			enumerateProcesses(v, pidv);
+
+			GroupsLen = (int)v.size();
+			for (int j = 0; j < GroupsLen; ++j) {
+				MenuItemEx item;
+				item.Clear();
+				item.strName = v[j].c_str();
+                item.AccelKey = 0;
+				item.Flags = 0;
+
+				ProcList.AddItem(&item);
+			}
+
+			ProcList.Show();
+			break;
+		case KEY_NUMDEL:
+		case KEY_DEL: 
+			kill(pidv[ProcList.GetSelectPos()], SIGTERM);
+			break;
+		default:
+			ProcList.ProcessInput();
+			break;
+		}
+	}
+}
+
+#else
+
 void ShowProcessList()
 {
 	farExecuteA(GetMyScriptQuoted("ps.sh").c_str(), 0);
@@ -59,57 +231,7 @@ void ShowProcessList()
 		FrameManager->RefreshFrame(i - 1);
 	}
 */
-#if 0
-	VMenu ProcList(Msg::ProcessListTitle,nullptr,0,ScrY-4);
-	ProcList.SetFlags(VMENU_WRAPMODE);
-	ProcList.SetPosition(-1,-1,0,0);
-
-//	if (!EnumWindows(EnumWindowsProc,(LPARAM)&ProcList))
-//		return;
-
-	ProcList.AssignHighlights(FALSE);
-	ProcList.SetBottomTitle(Msg::ProcessListBottom);
-	ProcList.Show();
-
-	while (!ProcList.Done())
-	{
-		FarKey Key=ProcList.ReadInput();
-
-		switch (Key)
-		{
-			case KEY_F1:
-			{
-				Help::Present(L"TaskList");
-				break;
-			}
-			case KEY_CTRLR:
-			{
-				ProcList.Hide();
-				ProcList.DeleteItems();
-				ProcList.SetPosition(-1,-1,0,0);
-
-//				if (!EnumWindows(EnumWindowsProc,(LPARAM)&ProcList))
-//				{
-//					ProcList.Modal::SetExitCode(-1);
-//					break;
-//				}
-
-				ProcList.Show();
-				break;
-			}
-			case KEY_NUMDEL:
-			case KEY_DEL:
-			{
-			}
-			break;
-			default:
-				ProcList.ProcessInput();
-				break;
-		}
-	}
-
-	if (ProcList.Modal::GetExitCode()>=0)
-	{
-	}
-#endif
 }
+
+#endif
+

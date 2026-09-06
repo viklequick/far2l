e887b4c8e6316f01b4397460ac033ba04e189db7
viklequick <viklequick@tut.by>
2026-09-06 15:49:12 +0200

    merged

--- far2l/src/options.cpp
+++ far2l/src/options.cpp
@@ -981,7 +981,7 @@ void ShellOptions(int LastCommand, MOUSE_EVENT_RECORD *MouseEvent)
 					FrameManager->ProcessKey(KEY_F12);
 					break;
 				case MENU_COMMANDS_PROCESSLIST:		// Task list
-					ShowProcessList(CtrlObject->Cp()->ActivePanel);
+					ShowProcessList(CtrlObject->Cp()->ActiveTab().ActivePanel);
 					break;
 				case MENU_COMMANDS_FARCONFIG:		// far:config
 					ConfigOptEdit();
